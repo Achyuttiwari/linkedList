5a893051cb7c59aa6ac6a0616168a471d58eedd4
Achyut Kumar Tiwari <achyuttiwari34@gmail.com>
2024-03-27 23:43:21 +0530

    Link the old list of the new node

--- src/node/NodeOperation.java
+++ src/node/NodeOperation.java
@@ -4,5 +4,6 @@ public class NodeOperation {
     public void pushNode(Node[] headRef, int dataValue){
         Node newNode = new Node();
         newNode.data = dataValue;
+        newNode.next = headRef[0];
     }
 }
